6a6717bb36a41765ab64cd465b9d734254acb3fb
yaeldiesel11 <astr2109@gmail.com>
2024-05-12 05:11:07 -0400

    commit

---
+++ src/componentes/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import List from './../List/List';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello Dojo! </h1>
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/List/List.jsx
@@ -0,0 +1,14 @@
+const List = () => {
+    return (
+        <div>
+            <li>
+                <h2>Things I need to do:</h2>
+                <ul>Learn React</ul>
+                <ul>Climb Mt. Everest</ul>
+                <ul>Run a marathon</ul>
+                <ul>Feed the dogs</ul>
+            </li>
+        </div>
+    );
+}
+export default List;
