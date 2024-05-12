e49077842a3ced25c14cffa323386a849193c93d
yaeldiesel11 <astr2109@gmail.com>
2024-05-12 05:47:24 -0400

    final commit

--- src/componentes/List/List.jsx
+++ src/componentes/List/List.jsx
@@ -1,13 +1,15 @@
+import './List.css';
+
 const List = () => {
     return (
         <div>
-            <li>
-                <h2>Things I need to do:</h2>
-                <ul>Learn React</ul>
-                <ul>Climb Mt. Everest</ul>
-                <ul>Run a marathon</ul>
-                <ul>Feed the dogs</ul>
-            </li>
+            <h2>Things I need to do:</h2>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
         </div>
     );
 }
